71a2742d49eca25b8899d74090afcfc869a223cc
keerthikumar <keerthikumar.cpp@gmail.com>
2023-09-20 19:13:42 +0530

    first change

---
+++ admin/user_header.php
@@ -0,0 +1,61 @@
+<?php
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
+?>
+
+<header class="header">
+
+   <section class="flex">
+
+      <a href="home.php" class="logo">Restro😋</a>
+
+   
+
+      <div class="icons">
+      
+         
+       
+         <a href="../index.html">Home</a>
+   
+         
+         <div id="menu-btn" class="fas fa-bars"></div>
+      </div>
+
+      <div class="profile">
+         <?php
+            $select_profile = $conn->prepare("SELECT * FROM `users` WHERE id = ?");
+            $select_profile->execute([$user_id]);
+            if($select_profile->rowCount() > 0){
+               $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+         ?>
+         <p class="name"><?= $fetch_profile['name']; ?></p>
+         <div class="flex">
+            <a href="profile.php" class="btn">profile</a>
+            <a href="components/user_logout.php" onclick="return confirm('logout from this website?');" class="delete-btn">logout</a>
+         </div>
+         <p class="account">
+            <a href="login.php">login</a> or
+            <a href="register.php">register</a>
+         </p> 
+         <?php
+            }else{
+         ?>
+            <p class="name">please login first!</p>
+            <a href="login.php" class="btn">login</a>
+         <?php
+          }
+         ?>
+      </div>
+
+   </section>
+
+</header>
+
---
+++ components/footer.php
@@ -0,0 +1,59 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:darshansm4022@gmail.com">darshansm4022@gmail.com</a>
+         <a href="mailto:keerthikumar.cpp@gmail.com">keerthikumar.cpp@gmail.com</a>
+         <a href="mailto:mekashreyareddy671@gmail.com">mkashreyareddy671@gmail.com</a>
+
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>Opening hours</h3>
+         <br>
+         <br>
+         <br>
+         <br>
+         <br>
+         
+         <p>06:00am to 10:00pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <br>
+    
+         <br>
+         <map>Welcome Restro, Koramangala 4th Block, Koramangala, Bengaluru, Karnataka 560038</map>
+  
+         <br>
+    
+         <br>
+         <a href="https://maps.app.goo.gl/UVtfzrQuJH4XxLq57">Click here Welcome Restro</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <br>
+         <br>
+        
+         <a href="tel:7625014022">+917625014022</a>
+         <a href="tel:9148717810">+919148717810</a>
+      </div>
+
+   </section>
+
+   <div class="credit">
+   <p>  <?= date('D:M:Y'); ?><p>
+</div>
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
